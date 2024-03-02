81e91be3326f10945b8707b703876ab7fcefbe6a
mi-fasol <minyoong39@naver.com>
2024-03-02 18:21:40 +0900

    feat: 이모티콘 할인 행사

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,109 +4,44 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    public static ArrayList<String> result;
-    public static int[] parent;
+    public static int[] discount = {10, 20, 30, 40};
+    public static ArrayList<int[]> res;
 
-    public static String[] solution(String[] commands) {
-        parent = new int[2500];
-        result = new ArrayList<>();
+    public static int[] solution(int[][] users, int[] emoticons) {
+        res = new ArrayList<>();
 
-        String[] table = new String[2500];
+        res.add(new int[]{0, 0});
 
-        for (int i = 0; i < 2500; i++) {
-            parent[i] = i;
-            table[i] = "";
-        }
+        dfs(users, emoticons, new int[emoticons.length], 0);
 
-        for (int i = 0; i < commands.length; i++) {
-            StringTokenizer st = new StringTokenizer(commands[i]);
-            String command = st.nextToken();
-            if (command.equals("UPDATE")) {
-                if (st.countTokens() == 2) {
-                    String value = st.nextToken();
-                    String change = st.nextToken();
-                    for (int j = 0; j < table.length; j++) {
-                        if (table[j].equals(value)) {
-                            table[j] = change;
-                        }
-                    }
+        Collections.sort(res, (a, b) -> b[0] - a[0] == 0 ? b[1] - a[1] : b[0] - a[0]);
+        return res.get(0);
+    }
+
+    public static void dfs(int[][] user, int[] emoticon, int[] dis, int cnt) {
+        if (cnt == emoticon.length) {
+            int plusUser = 0;
+            int emoticonSelling = 0;
+
+            for (int i = 0; i < user.length; i++) {
+                int sum = 0;
+                for (int j = 0; j < emoticon.length; j++) {
+                    if (dis[j] >= user[i][0])
+                        sum += emoticon[j] / 100 * (100 - dis[j]);
+                }
+                if (sum >= user[i][1]) {
+                    plusUser++;
                 } else {
-                    int r = Integer.parseInt(st.nextToken());
-                    int c = Integer.parseInt(st.nextToken());
-                    String value = st.nextToken();
-                    int root = find(changeIndex(r, c));
-                    for (int j = 0; j < table.length; j++) {
-                        if (find(j) == root) {
-                            table[j] = value;
-                        }
-                    }
+                    emoticonSelling += sum;
                 }
-            } else if (command.equals("MERGE")) {
-                int r1 = Integer.parseInt(st.nextToken());
-                int c1 = Integer.parseInt(st.nextToken());
-                int r2 = Integer.parseInt(st.nextToken());
-                int c2 = Integer.parseInt(st.nextToken());
-                int root1 = find(changeIndex(r1, c1));
-                int root2 = find(changeIndex(r2, c2));
-
-                String value = table[root1].equals("") ? table[root2] : table[root1];
-
-                if (root1 == root2) {
-                    continue;
-                }
-
-                union(root1, root2);
-
-                for (int j = 0; j < 2500; j++) {
-                    if (find(j) == root2) {
-                        table[j] = table[root1];
-                        parent[j] = root1;
-                    }
-                }
-
-                table[changeIndex(r1, c1)] = value;
-                table[changeIndex(r2, c2)] = value;
-            } else if (command.equals("UNMERGE")) {
-                int r1 = Integer.parseInt(st.nextToken());
-                int c1 = Integer.parseInt(st.nextToken());
-                int root = find(changeIndex(r1, c1));
-
-                String value = table[root];
-                for (int j = 0; j < 2500; j++) {
-                    if (find(j) == root) {
-                        table[j] = "";
-                        parent[j] = j;
-                    }
-                }
-                if (value != null)
-                    table[changeIndex(r1, c1)] = value;
-            } else {
-                int r = Integer.parseInt(st.nextToken());
-                int c = Integer.parseInt(st.nextToken());
-
-                int root = find(changeIndex(r, c));
-                String rs = table[root];
-                result.add(Objects.equals(rs, "") ? "EMPTY" : rs);
+            }
+            res.add(new int[]{plusUser, emoticonSelling});
+            return;
+        } else {
+            for (int i = 0; i < 4; i++) {
+                dis[cnt] = discount[i];
+                dfs(user, emoticon, dis, cnt + 1);
             }
         }
-        return result.toArray(new String[0]);
-    }
-
-    public static int find(int a) {
-        if (parent[a] == a)
-            return a;
-        else
-            return parent[a] = find(parent[a]);
-    }
-
-    public static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b)
-            parent[b] = a;
-    }
-
-    public static int changeIndex(int r, int c) {
-        return (r - 1) * 50 + (c - 1);
     }
 }

76f6d8c6cbcb3903b0a5b79c30e0039d0f71cfe7
mi-fasol <minyoong39@naver.com>
2024-03-07 16:03:12 +0900

    feat: 두 큐 합 같게 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,57 @@
 package org.example;
 
-import java.io.*;
-import java.util.*;
+import java.io.;
+import java.util.;
 
 class Solution {
-    static char[] typeList = {'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'};
+    public static int solution(int[] queue1, int[] queue2) {
+        int result = 0;
 
-    public static String solution(String[] survey, int[] choices) {
-        StringBuilder result = new StringBuilder();
+        Deque<Long> q1 = new ArrayDeque<>();
+        Deque<Long> q2 = new ArrayDeque<>();
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        long q1Sum = 0;
+        long q2Sum = 0;
 
-        for (char i : typeList) {
-            map.put(i, 0);
+        for (int queue : queue1) {
+            q1.add((long) queue);
+            q1Sum += queue;
         }
 
-        for (int i = 0; i < survey.length; i++) {
-            char[] surveyChar = survey[i].toCharArray();
-            if (choices[i] < 4) map.put(surveyChar[0], map.get(surveyChar[0]) + (4 - choices[i]));
-            else if (choices[i] > 4) map.put(surveyChar[1], map.get(surveyChar[1]) + (choices[i] - 4));
-            else continue;
+        long size = q1.size();
+
+        for (int queue : queue2) {
+            q2.add((long) queue);
+            q2Sum += queue;
         }
 
-        for(int i = 0; i <4; i++){
-            if(map.get(typeList[i*2]) < map.get(typeList[i*2 + 1])){
-                result.append(typeList[i * 2 + 1]);
-            } else{
-                result.append(typeList[i * 2]);
+        while (q1Sum != q2Sum) {
+
+            result++;
+
+            if (q1Sum > q2Sum) {
+                long next = q1.poll();
+                q1Sum -= next;
+                q2Sum += next;
+                q2.offer(next);
+            } else {
+                long next = q2.poll();
+                q2Sum -= next;
+                q1Sum += next;
+                q1.offer(next);
             }
+
+            if (q1Sum == q2Sum) {
+                break;
+            }
+
+            if (result > size * 4) {
+                result = -1;
+                break;
+            }
+
         }
 
-        return result.toString();
+        return result;
     }
 }

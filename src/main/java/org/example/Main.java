8a490266e9e7f6ba65968c34e6eec3a1f92bcb8b
mi-fasol <minyoong39@naver.com>
2024-04-01 20:35:11 +0900

    feat: 호텔 대실 (lv.2)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,84 +4,47 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+    public static int solution(String[][] book_time) {
+        HashMap<Integer, List<Integer>> book = new HashMap<>();
 
-    static int[][] b;
-    static int INF = 987654321;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
+        for (int i = 0; i < book_time.length; i++) {
+            String start = book_time[i][0];
+            String end = book_time[i][1];
 
-    public static int solution(int[][] board, int[] aloc, int[] bloc) {
+            int start_time = Integer.parseInt(start.substring(0, 2)) * 60 + Integer.parseInt(start.substring(3));
+            int end_time = Integer.parseInt(end.substring(0, 2)) * 60 + Integer.parseInt(end.substring(3));
 
-        b = board;
-
-        return Math.abs(dfs(aloc[0], aloc[1], bloc[0], bloc[1], true));
-    }
-
-    public static int dfs(int ax, int ay, int bx, int by, boolean turn) {
-        int x = turn ? ax : bx;
-        int y = turn ? ay : by;
-
-        List<Integer> list = new ArrayList<>();
-        for (int d = 0; d < 4; d++) {
-            int fx = x + dx[d];
-            int fy = y + dy[d];
-
-            // 이동 불가한 곳이면 continue
-            if (fx < 0 || fy < 0 || fx >= b.length || fy >= b[0].length || b[fx][fy] == 0) continue;
-
-            // A와 B가 같은 위치에 있는 경우
-            if (ax == bx && ay == by) {
-                list.add(1);
-                continue;
-            }
-
-            // 방문 표시 - 발판이 사라짐
-            b[x][y] = 0;
-
-            int res;
-
-            // A의 차례인 경우 B의 차례로 변경
-            if (turn) res = -dfs(fx, fy, bx, by, !turn);
-
-            // B의 차례인 경우 A의 차례로 변경
-            else res = -dfs(ax, ay, fx, fy, !turn);
-
-            // 결과값이 0보다 크거나 같음면 +1, 아니면 -1
-            if (res >= 0) res++;
-            else res--;
-
-            // 결과값을 list에 추가
-            list.add(res);
-
-            // 백트래킹을 위해 다시 1로 설정 - x, y를 밟지 않고 넘어갈 경우
-            b[x][y] = 1;
+            book.put(i, new ArrayList<>(Arrays.asList(start_time, end_time)));
         }
 
-        int pMax, pMin, mMax, mMin;
-        // 최대 최소 초기화
-        pMax = mMax = -INF;
-        pMin = mMin = INF;
+        List<Integer> keySet = new ArrayList<>(book.keySet());
 
-        for (int i = 0; i < list.size(); i++) {
-            int num = list.get(i);
+        keySet.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return book.get(o1).get(0).compareTo(book.get(o2).get(0));
+            }
+        });
 
-            // list에서 꺼내온 결과가 양수면 p변수 갱신, 음수면 m변수 갱신
-            if (num > 0) {
-                pMax = Math.max(pMax, num);
-                pMin = Math.min(pMin, num);
-            } else {
-                mMax = Math.max(mMax, num);
-                mMin = Math.min(mMin, num);
+        ArrayList<Boolean> room = new ArrayList<>();
+        room.add(true);
+        ArrayList<Integer> who = new ArrayList<>();
+        who.add(keySet.get(0));
+
+        for (int i = 1; i < keySet.size(); i++) {
+            boolean isPossible = false;
+            for (int j = 0; j < room.size(); j++) {
+                if (book.get(keySet.get(i)).get(0) >= book.get(who.get(j)).get(1) + 10) {
+                    who.set(j, keySet.get(i));
+                    isPossible = true;
+                    break;
+                }
+            }
+            if (!isPossible) {
+                room.add(true);
+                who.add(keySet.get(i));
             }
         }
-
-        // 둘 다 움직일 수 없었던 경우 0 반환
-        if (pMax == -INF && mMax == -INF) return 0;
-        // pMax가 갱신되지 않았다면 B가 이긴 경우 -> mMin 반환
-        else if (pMax == -INF) return mMin;
-        // pMax가 갱신되었다면 A가 이긴 경우 -> pMin 반환
-        else if (pMax != -INF) return pMin;
-        // 예외 처리
-        else return 0;
+        return room.size();
     }
 }
